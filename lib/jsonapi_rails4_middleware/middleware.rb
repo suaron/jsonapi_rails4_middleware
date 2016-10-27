2f706de2fae31df26c6c36579a2a3894ce3e5c16
Dmytro Piliugin <dmytro.piliugin@gmail.com>
2016-10-28 02:49:41 +0300

    fix 2

--- lib/jsonapi_rails4_middleware/middleware.rb
+++ lib/jsonapi_rails4_middleware/middleware.rb
@@ -5,25 +5,25 @@ module JsonapiRails4Middleware
     end
 
     def call(env)
-      request = Rack::Request.new(env)
+      if env['CONTENT_TYPE'] == 'application/json'
+        env['rack.input'].rewind
+        body = env['rack.input'].read
+        env['rack.input'].rewind
 
-      if request.content_type == 'application/json'
-        request.body.rewind
-        body = request.body.read
-        request.body.rewind
-        json_params = body.empty? ? {} : JSON.parse(body)
-        data = json_params.fetch('data', nil)
-        attributes = data['attributes']
-        type = data['type']
-        jsonapi =  data && attributes && type
+        (body.empty? ? {} : JSON.parse(body)).tap do |json_params|
+          break unless data = json_params['data']
 
-        if jsonapi
+          attributes = data['attributes']
+          type = data['type']
+          break unless attributes && type
+
+          if id = data['id']
+            attributes['id'] = id
+          end
+
           resource_name = type.singularize
-          params = {}
-          params[resource_name] = attributes
-          id = data['id']
-          params[resource_name]['id'] = id if id
-          env['rack.input'] = StringIO.new(params.to_json)
+
+          env['rack.input'] = StringIO.new({ resource_name => attributes }.to_json)
         end
       end
 
